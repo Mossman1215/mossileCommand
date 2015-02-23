6da8eac5f6c544a56e0bd98b230b90c6fc2d8e2a
Moss Cantwell <mossman2142@gmail.com>
2015-02-23 16:39:43 +1300

    fixed targeting of missiles

--- core/src/com/fruitsofdoom/MossileCommand/Missile.java
+++ core/src/com/fruitsofdoom/MossileCommand/Missile.java
@@ -2,6 +2,7 @@ package com.fruitsofdoom.MossileCommand;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Pool.Poolable;
 
@@ -11,25 +12,23 @@ public class Missile {
 	Vector2 position;
 	boolean visible;
 	Vector2 startPt;
-
+	Vector2 speed;
+	Vector2 difference;
 	public Missile(Vector2 touch, int vpWidth) {
 		visible = true;
 		this.touch = touch;
 		viewPortWidth = vpWidth;
 		position = new Vector2(viewPortWidth / 2, 0);
 		startPt = new Vector2(viewPortWidth / 2, 0);
+		speed = new Vector2(50,50);
+		difference = new Vector2();
+		difference = touch.sub(position);
+		difference = difference.nor();
+		speed = speed.scl(difference);
 	}
 
 	public void update() {
-		if (touch.x > position.x) {
-			position.x += 100 * Gdx.graphics.getDeltaTime();
-		}
-		if (touch.x < position.x) {
-			position.x -= 100 * Gdx.graphics.getDeltaTime();
-		}
-		if (touch.y > position.y) {
-			position.y += 100 * Gdx.graphics.getDeltaTime();
-		}
+		position.mulAdd(speed, Gdx.graphics.getDeltaTime());
 		if (touch.dst(position) < 10) {
 			// explode
 			visible = false;
