cf0f25524b8f9a59127e73737d3375312a247ec4
Moss Cantwell <mossman2142@gmail.com>
2015-03-15 10:51:05 +1300

    the game is winnable

--- core/src/com/fruitsofdoom/MossileCommand/Spawner.java
+++ core/src/com/fruitsofdoom/MossileCommand/Spawner.java
@@ -10,23 +10,26 @@ public class Spawner {
 	LinkedList<ICBM> ICBMList;
 	int maxAmt;
 	boolean complete;
+	int runningTotal;
 	public Spawner(float delay,LinkedList<ICBM> list,int maxAmt){
 		this.delay = delay;
 		currentTime = 0;
 		ICBMList = list;
 		this.maxAmt = maxAmt;
 		complete= false;
+		runningTotal = 0;
 	}
 	public void Update(){
 		float delta = Gdx.graphics.getDeltaTime();
 		currentTime+=delta;
 		if(currentTime>delay){
 			currentTime =0;
-			if(ICBMList.size()<maxAmt){
+			if(runningTotal<maxAmt){
 				ICBMList.add(new ICBM(1));
+				runningTotal++;
 			}
 		}
-		if(ICBMList.size()==maxAmt){
+		if(runningTotal==maxAmt){
 			complete=true;
 		}
 	}
